fd0111b3ab36dddd2756942e19ed8d59aea6d53f
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-07-20 13:36:42 -0500

    Build it out

---
+++ sonnet.js
@@ -0,0 +1,11 @@
+let s = document.querySelector('#sonnet').innerText;
+let s1 = document.querySelector('#sonnet');
+
+function do_stuff() {
+    let index = s.indexOf();
+    let count = s.length;
+    s1.innerHTML = s.replace('winter', 'yuletide').replace(/ The & the /g, 'a large')
+
+}
+
+do_stuff()
